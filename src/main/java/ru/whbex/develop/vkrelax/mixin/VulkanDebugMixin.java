49917c54b3bac64a7173ff9e933107207db11e7d
whitebelyash <whbexiumwork@gmail.com>
2026-06-07 18:08:38 +0400

    Feat[mixin]: add a workaround for QUALCOMM_PROPRIETARY

---
+++ src/main/java/ru/whbex/develop/vkrelax/mixin/VulkanDebugMixin.java
@@ -0,0 +1,18 @@
+package ru.whbex.develop.vkrelax.mixin;
+
+import com.mojang.blaze3d.vulkan.VulkanDebug;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
+import ru.whbex.develop.vkrelax.VkRelax;
+
+import java.util.Set;
+
+@Mixin(VulkanDebug.class)
+public interface VulkanDebugMixin {
+
+    @Overwrite
+    static VulkanDebug create(final int verbosity, final boolean wantsDebugLabels, final Set<String> availableExtensions, final Set<String> enabledExtensions){
+        VkRelax.LOGGER.info("Returning disabled Debug");
+        return new VulkanDebug.Disabled();
+    }
+}
